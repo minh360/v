b58c7c6d4e0f0419780b63fd7f2b5148fdf02970
minh360 <minhtrai23112001@gmail.com>
2024-04-12 23:28:33 +0700

    commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,33 @@
-
 <template>
   <div class="w-full h-full bg-[#3399FF] text-[blue] background">
-    <div class="flex justify-center items-center h-full gap-[20px] uppercase ">
-      <div class=" text-[50px] bg-opacity-0 rounded-[20px] border border-solid border-4 border-red-800 p-[10px]">Đăng nhập</div>
-      <div class=" text-[50px] bg-opacity-0 rounded-[20px] border border-solid border-4 border-red-800 p-[10px]">Đăng ký</div>
+    <div class="flex justify-center items-center h-full gap-[20px] uppercase " v-if="status == STATUS.FREE">
+      <div class="hover:bg-[#7195AF] cursor-pointer text-[50px] bg-opacity-0 
+        rounded-[20px] border border-solid border-4 border-red-800 p-[10px]">
+        Đăng nhập
+      </div>
+      <div class="hover:bg-[#7195AF] cursor-pointer text-[50px] bg-opacity-0 
+        rounded-[20px] border border-solid border-4 border-red-800 p-[10px]">
+        Đăng ký
+      </div>
+    </div>
+    <div v-if="status == STATUS.SIGN_IN">
+      
     </div>
   </div>
-  
+
 </template>
 
 <script setup>
-
+import { ref } from 'vue'
+const STATUS = {
+  FREE: 0,
+  SIGN_IN: 1,
+  SIGN_UP: 2
+}
+const status = ref(STATUS.FREE)
 </script>
 <style scoped>
-.background{
+.background {
   background-image: url(../assets/bg.jpg);
   background-repeat: no-repeat;
   background-size: cover;
