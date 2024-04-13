e71ebdb66d865dceda92b086fd732e01a5fe8e37
minh360 <minhtrai23112001@gmail.com>
2024-04-13 16:25:42 +0700

    commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="w-full h-full bg-[#3399FF] text-[blue] background">
-    <div class="flex justify-center items-center h-full gap-[20px] uppercase " v-if="status == STATUS.FREE">
-      <div class="hover:bg-[#7195AF] cursor-pointer text-[50px] bg-opacity-0 
-        rounded-[20px] border border-solid border-4 border-red-800 p-[10px]">
-        Đăng nhập
-      </div>
-      <div class="hover:bg-[#7195AF] cursor-pointer text-[50px] bg-opacity-0 
-        rounded-[20px] border border-solid border-4 border-red-800 p-[10px]">
-        Đăng ký
+  <div class="w-full h-full bg-[#3399FF] background ">
+    <div class="flex justify-center items-center h-full text-[50px] uppercase text-[blue]">
+      <div class="hover:bg-[#7195AF] cursor-pointer bg-opacity-0 
+        rounded-[20px] border border-solid border-4 border-red-800 p-[10px]" @click="setStatus(STATUS.SIGN_UP)">
+        Bắt đầu
       </div>
     </div>
-    <div v-if="status == STATUS.SIGN_IN">
-      
+    <div class="absolute backdrop-blur w-full h-full top-0" v-if="status != STATUS.FREE">
+      <div
+        class="mt-[100px] text-white border border-1 border-[black] rounded-[25px] w-[600px] h-auto mx-auto">
+        <div class="uppercase p-[20px] w-full h-[60px] flex justify-around text-[35px]">
+          <div class="cursor-pointer font-bold" :class="{ active: STATUS.SIGN_UP == status }"
+            @click="setStatus(STATUS.SIGN_UP)">
+            Đăng ký
+          </div>
+          <div class="cursor-pointer font-bold" :class="{ active: STATUS.SIGN_IN == status }"
+            @click="setStatus(STATUS.SIGN_IN)">
+            Đăng nhập
+          </div>
+        </div>
+        <div class=" mt-[30px] h-full border border-0 rounded-[25px]">
+          <SignIn v-if="STATUS.SIGN_IN == status" />
+          <SignUp v-if="STATUS.SIGN_UP == status" />
+        </div>
+      </div>
     </div>
   </div>
 
@@ -19,12 +31,17 @@
 
 <script setup>
 import { ref } from 'vue'
+import SignIn from './SignIn.vue'
+import SignUp from './SignUp.vue'
 const STATUS = {
   FREE: 0,
   SIGN_IN: 1,
   SIGN_UP: 2
 }
 const status = ref(STATUS.FREE)
+const setStatus = num => {
+  status.value = num
+}
 </script>
 <style scoped>
 .background {
@@ -32,5 +49,9 @@ const status = ref(STATUS.FREE)
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+}
+
+.active {
+  color: rebeccapurple
 }
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="w-full h-full text-white">
+  <form class=" rounded px-8 pt-6 pb-8 mb-4 h-full flex flex-col ">
+    <div class="mb-4">
+      <label class="block text-sm font-bold mb-2" for="username">
+        Ingame
+      </label>
+      <input v-model="ingame" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
+      leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Ingame">
+    </div>
+    <div class="mb-6">
+      <label class="block text-sm font-bold mb-2" for="password">
+        Mật khẩu
+      </label>
+      <input v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
+      mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+    </div>
+    <p v-if="message != ''">{{ message }}</p>
+    <div class="flex items-center justify-center">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 
+      rounded focus:outline-none focus:shadow-outline" type="button" @click="signUp()">
+        Đăng ký
+      </button>
+    </div>
+  </form>
+</div>
+</template>
+<script setup>
+import {ref} from 'vue'
+const ingame = ref('')
+const password = ref('')
+const message = ref('')
+const signUp = () => {
+    
+}
+</script>
+<style scoped lang="scss">
+
+</style>
