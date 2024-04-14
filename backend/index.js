7c0b81f65c77c9c43ad7584a5abcf7159854c37f
minh360 <minhtrai23112001@gmail.com>
2024-04-14 23:26:37 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,17 @@ const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json();
-const MongoClient = require ('mongodb').MongoClient
 const app = express()
 const path = require('path')
 const serveStatic = require('serve-static')
 const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
 const dayjs = require("dayjs");
+const router = require('./router')
 app.use(cors())
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-mongoose.connect('mongodb+srv://minh231120012:setdanh113@cluster0.wuz4vl8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://minh231120012:setdanh113@cluster0.wuz4vl8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/vxmm')
 
 const db = mongoose.connection;
 db.once('open', function(){
